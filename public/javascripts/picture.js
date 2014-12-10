9482f0c66326d72f6f3d7943ab2955413b9bde26
guillaumeschweitzer <guillaume.schweitzer@ysance.com>
2014-12-10 17:57:53 +0100

    ajout fonctionnalité takePicture

--- public/javascripts/picture.js
+++ public/javascripts/picture.js
@@ -4,35 +4,82 @@ function getPicture(path){
 
 };
 
-// $(document).on('click', 'div button', function(){
-//     var fileName = $('input#path').val();
-//     postRequestToGetPicture(fileName, function(error, result) {
-//         if (!error) 
-//             console.log(result, typeof result);
-//         	// on peut récupérer un fichier 
-//     });
-// });
+var pattern = {
+	fileName : "",
+	width : "",
+	height : "",
+	quality : "",
+	rot : ""
+}
 
-$(document).on('click', 'div button', function(){
-    var fileName = $('input#path').val();
-
-    // Check fileName ! 
-
-    postRequestToTakePicture(fileName, function(error, result) {
-        if (!error) {
-            // console.log(result, typeof result);
-        	postRequestToCopyPicture(function(err, res){
-        		if (!err) {
-		        	$('div#linkToDisplay').append("<a href='/showPicture'>Show me Picture</a>");
-		        	// Quand la photo est prise, on affiche ok + un lien vers showPicture ! 
-		        	// Display link ! 
-        		}
-        		else 
-        			alert('erreur dans la copie du fichier');
-        	})
-        }
-        else {
-        	alert("Erreur dans la caputre image : '", error, "', '", result,"'");
-        }
+$(document).on('click', 'div button#ok', function(){
+    // if($('div#optionList').css('display') != 'none')
+    // 	getAdvancedOptions();
+    // else 
+    var vals = getSimpleOptions();
+    // var args = $('input#path').val();
+    // console.log("Arguments envoyés : ", args);
+    postRequestToGetPicture(vals, function(error, result) {
+        if (!error) 
+            console.log(result, typeof result);
     });
-});
+})
+
+$(document).on('click', 'div button#options', function() {
+	$('#optionList').toggle();
+})
+
+function getValues(name) {
+	var label = '#'+name;
+	var val = $(label).val();
+	console.log(label, val);
+	return val;
+}
+
+function checkLength(val) {
+	if (val.length > 0)
+		return true
+	else 
+		return false
+}
+
+function getAdvancedOptions() {
+	var optionList = [{label:"fileName", command:"-o"},
+	{label:"width", command:"-w"},
+	{label:"height",commande:"-h"},
+	{label:"quality",commande:"-q"},
+	{label:"timeout",commande:"-t"},
+	{label:"timestamp",commande:"-tl"},
+	{label:"rotation",commande:"-rot"},
+	{label:"sharpness",commande:"-sh"},
+	{label:"contrast",commande:"-co"},
+	{label:"brightness",commande:"-br"},
+	{label:"saturation",commande:"-sa"},
+	{label:"iso",commande:"-iso"},
+	{label:"verticalFlip",commande:"-vf"},
+	{label:"horizontalFlip",commande:"-hf"},
+	{label:"encoding",commande:"-e"},
+	{label:"exposure",commande:"-ex"},
+	{label:"awb",commande:"-awb"},
+	{label:"imageEffect",commande:"-ifx"},
+	{label:"metering",commande:"-mm"},
+	{label:"drc",commande:"-drc"},
+	{label:"shutter",commande:"ss"}]
+}
+
+function getSimpleOptions() {
+	var optionList = [{label:"fileName", commande:"-o"},
+	{label:"width", commande:"-w"},
+	{label:"height",commande:"-h"},
+	{label:"quality",commande:"-q"},
+	{label:"encoding", commande:"-e"}]
+	for (var i in optionList){
+		optionList[i].value = typeof getValues(optionList[i].label) != "boolean" ? getValues(optionList[i].label) : false;
+	}
+	var args = [];
+	for (var i in optionList) {
+			args.push(""+optionList[i].commande+" "+optionList[i].value)
+	}
+	console.log(args.join(' '));
+	return args;
+}
